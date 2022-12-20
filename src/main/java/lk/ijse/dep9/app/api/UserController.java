1e39d39a5d79aa80289638b9a0a79a5d43f2dadc
Sanduni Nagodage <sanduninagodage@gmail.com>
2022-12-20 18:09:23 +0530

    Add UserDT

---
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -0,0 +1,27 @@
+package lk.ijse.dep9.app.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @PostMapping
+    public void createUserAccount(){
+
+    }
+    @PatchMapping("/me")
+    public void updateUserAccountDetail(){
+
+    }
+
+    @GetMapping("/me")
+    public void getUserAccountDetails(){
+
+    }
+
+    @DeleteMapping("/me")
+    public void deleteUserAccount(){
+
+    }
+}
