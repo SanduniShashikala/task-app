44f78870d127f381a52f9647cc1936f0c351f166
Sanduni Nagodage <sanduninagodage@gmail.com>
2022-12-22 13:41:32 +0530

    Implement deleteUserAccount in UserServiceImpl

--- src/main/java/lk/ijse/dep9/app/service/custom/UserService.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/UserService.java
@@ -11,4 +11,6 @@ public interface UserService extends SuperService {
     UserDTO getUserAccountDetails(String username);
 
     void updateUserAccountDetails(UserDTO userDTO);
+
+    void deleteUserAccount(String username);
 }
