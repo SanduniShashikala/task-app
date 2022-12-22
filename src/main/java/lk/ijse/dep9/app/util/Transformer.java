f6fd1ced8ce1c1210103c1a1cef24cb28b238451
Sanduni Nagodage <sanduninagodage@gmail.com>
2022-12-22 14:18:55 +0530

    Implement Entity DTO convention

--- src/main/java/lk/ijse/dep9/app/util/Transformer.java
+++ src/main/java/lk/ijse/dep9/app/util/Transformer.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep9.app.util;
 
+import lk.ijse.dep9.app.dto.ProjectDTO;
 import lk.ijse.dep9.app.dto.UserDTO;
+import lk.ijse.dep9.app.entity.Project;
 import lk.ijse.dep9.app.entity.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,4 +20,11 @@ public class Transformer {
         return mapper.map(dto, User.class);
     }
     public UserDTO toUserDTO(User user){return mapper.map(user, UserDTO.class);}
+
+    public Project toProject(ProjectDTO dto){
+        return mapper.map(dto, Project.class);
+    }
+    public ProjectDTO toProjectDTO(Project projectEntity){
+        return mapper.map(projectEntity, ProjectDTO.class);
+    }
 }
