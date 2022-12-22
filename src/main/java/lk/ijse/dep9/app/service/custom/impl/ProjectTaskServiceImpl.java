e40d1bc4be76231553c18dd1110df785a0042df9
Sanduni Nagodage <sanduninagodage@gmail.com>
2022-12-22 14:39:18 +0530

    Implement ProjectTaskServiceImpl

--- src/main/java/lk/ijse/dep9/app/service/custom/impl/ProjectTaskServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/impl/ProjectTaskServiceImpl.java
@@ -3,12 +3,18 @@ package lk.ijse.dep9.app.service.custom.impl;
 import lk.ijse.dep9.app.dao.custom.ProjectDAO;
 import lk.ijse.dep9.app.dao.custom.TaskDAO;
 import lk.ijse.dep9.app.dto.ProjectDTO;
+import lk.ijse.dep9.app.entity.Project;
+import lk.ijse.dep9.app.exception.AccessDeniedException;
 import lk.ijse.dep9.app.service.custom.ProjectTaskService;
+import lk.ijse.dep9.app.util.Transformer;
 import org.springframework.context.annotation.Scope;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional
 @Component
@@ -16,33 +22,43 @@ public class ProjectTaskServiceImpl implements ProjectTaskService {
     private ProjectDAO projectDAO;
     private TaskDAO taskDAO;
 
-    public ProjectTaskServiceImpl(ProjectDAO projectDAO, TaskDAO taskDAO) {
+    private Transformer transformer;
+
+    public ProjectTaskServiceImpl(ProjectDAO projectDAO, TaskDAO taskDAO, Transformer transformer) {
         this.projectDAO = projectDAO;
         this.taskDAO = taskDAO;
+        this.transformer = transformer;
     }
 
     @Override
     public ProjectDTO createNewProject(ProjectDTO projectDTO) {
-        return null;
+        return transformer.toProjectDTO(projectDAO.save(transformer.toProject(projectDTO)));
     }
 
     @Override
     public List<ProjectDTO> getAllProjects(String username) {
-        return null;
+        return projectDAO.findAllProjectsByUsername(username).stream().map(transformer::toProjectDTO).collect(Collectors.toList());
     }
 
     @Override
     public ProjectDTO getProjectDetails(String username, int projectId) {
-        return null;
+        ProjectDTO project = projectDAO.findById(projectId).map(transformer::toProjectDTO).orElseThrow(()-> new EmptyResultDataAccessException(1));
+        if (!project.getUsername().matches(username))throw new AccessDeniedException();
+        return project;
     }
 
     @Override
     public void renameProject(ProjectDTO project) {
-
+        Project projectEntity = projectDAO.findById(project.getId()).orElseThrow(()-> new EmptyResultDataAccessException(1));
+        if (!projectEntity.getUsername().matches(project.getUsername())) throw new AccessDeniedException();
+        projectDAO.update(transformer.toProject(project));
     }
 
     @Override
     public void deleteProject(String username, int projectId) {
-
+        Project project = projectDAO.findById(projectId).orElseThrow(()-> new EmptyResultDataAccessException(1));
+        if (!project.getUsername().matches(username)) throw new AccessDeniedException();
+        taskDAO.findAllTaskByProjectId(projectId).forEach(task -> taskDAO.deleteById(task.getId()));
+        projectDAO.deleteById(projectId);
     }
 }
